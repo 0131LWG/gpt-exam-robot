64b0e123c67d9bb8057128a126f2f933ece053b2
刘启明 <455043818@qq.com>
2023-07-25 13:49:54 +0800

    feat: 解决冲突

--- client/src/pages/api/dictionary/group/list.ts
+++ client/src/pages/api/dictionary/group/list.ts
@@ -41,10 +41,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     ]);
 
     jsonRes(<any>res, {
-      pageNum: pageNum,
-      pageSize,
-      data: models,
-      total
+      data: {
+        pageNum: pageNum,
+        pageSize,
+        data: models,
+        total
+      }
     });
 
     // jsonRes(res, {
